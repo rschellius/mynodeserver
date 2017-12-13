61d15cf42a6b49153d3212a05223dd1a66f88074
Robin Schellius <r.schellius@avans.nl>
2017-12-13 14:40:03 +0100

    Updated

--- api/routes_v1.js
+++ api/routes_v1.js
@@ -4,25 +4,36 @@ var routes = express.Router();
 
 module.exports = {}
 
-var mijnObject = { 
+var mijnObject = {
 	mijntekst: 'Hello World!'
 };
 
-routes.get('/hello', function(req, res){
-	res.contentType('application/json');
+routes.get('/hello', function (req, res) {
 	res.status(200);
 	res.json(mijnObject);
 });
 
-routes.post('/hello', function(req, res){
+routes.post('/hello', function (req, res) {
 	var body = req.body;
-	
+
 	console.dir(body);
 
-	res.contentType('application/json');
 	res.status(200);
 	res.json(mijnObject);
 
+});
+
+routes.get('/hello/error', function (req, res, next) {
+	next('HIER TREEDT EEN ERROR OP');
+});
+
+
+routes.get('/hello/*', function (req, res, next) {
+	res.status(404);
+	res.json({
+		message: 'Deze endpoint bestaat nog niet'
+	});
+	res.end();
 });
 
 module.exports = routes;
