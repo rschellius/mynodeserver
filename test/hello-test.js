e4924974a68f4dbd709495a076eff3bb3e314400
Robin Schellius <r.schellius@avans.nl>
2017-12-13 15:46:59 +0100

    Updated

--- test/hello-test.js
+++ test/hello-test.js
@@ -5,8 +5,9 @@ var chould = chai.should();
 
 chai.use(chaiHttp);
 
-describe('Hello', function () {
-    it('GET /api/v1/hello', function (done) {
+describe('Hello API interface', function () {
+
+    it('should GET /api/v1/hello correctly', function (done) {
         chai.request(server)
             .get('/api/v1/hello')
             .end(function (err, res) {
@@ -15,5 +16,19 @@ describe('Hello', function () {
                 // res.body
                 done();
             });
-    })
+    });
+
+    it('should handle /api/v1/hello/error correctly', function (done) {
+        chai.request(server)
+            .get('/api/v1/hello/error')
+            .end(function (err, res) {
+                res.should.have.status(500);
+                res.should.be.json;
+                res.body.should.be.a('object');
+                res.body.should.have.property('message');
+                res.body.message.should.equal('HIER TREEDT EEN ERROR OP');
+                done();
+            });
+    });
+
 });
