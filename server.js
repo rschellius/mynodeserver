33b086d076849b703eddb961bf53b6f00d9f7619
Robin Schellius <r.schellius@avans.nl>
2017-12-19 08:49:41 +0100

    Toelichting toegevoegd

--- server.js
+++ server.js
@@ -14,13 +14,14 @@ var app = express();
 app.use(bodyParser.urlencoded({
 	'extended': 'true'
 })); // parse application/x-www-form-urlencoded
-app.use(bodyParser.json()); // parse application/json
+app.use(bodyParser.json());
 app.use(bodyParser.json({
 	type: 'application/vnd.api+json'
 })); // parse application/vnd.api+json as json
 
 app.use(logger('dev'));
 
+// Voeg ContentType toe aan alle responses (en ga door naar next route handler)
 app.use('*', function (req, res, next) {
 	res.contentType('application/json');
 	console.log('contenttype toegevoegd.');
@@ -28,14 +29,18 @@ app.use('*', function (req, res, next) {
 	next();
 });
 
+// Demo route handler - print logregel voor alle /api* endpoints.
 app.use('/api*', function (req, resp, next) {
 	console.log('/api aangeroepen');
 	next();
 });
 
+// Installeer de api endpoint routes die we willen aanbieden 
 app.use('/api/v1', routes_v1);
 app.use('/api/v1', todos_v1);
 
+// Logregel, wordt getoond wanneer geen andere routes matchten
+// EN er geen foutsituatie is - anders wordt de error handler aangeroepen  
 app.use('*', function (req, res, next) {
 	res.status(200)
 		.json({
@@ -44,8 +49,9 @@ app.use('*', function (req, res, next) {
 		.end();
 });
 
+// Error handler, handelt alle foutsituaties af waarbij error !== null
 app.use(function (error, req, res, next) {
-	console.log('Error: ' + error);
+	console.error(error.toString());
 	res.status(500).json({
 		message: error
 	}).end();
